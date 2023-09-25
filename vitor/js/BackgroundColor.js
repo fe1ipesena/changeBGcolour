81309388a1522b25e9114238d799e7b916dfd10e
Gustavo <gugaro8@gmail.com>
2023-09-24 23:49:23 -0400

    atualizado

--- vitor/js/BackgroundColor.js
+++ vitor/js/BackgroundColor.js
@@ -1,6 +1,23 @@
-function changeColor(event) {
-  var color = event.value;
-  document.getElementsByTagName('BODY')[0].style.backgroundColor = color;
+document.addEventListener("DOMContentLoaded", function () {
+  // Llama a la función para cargar el color al cargar la página
+  loadColor();
+
+  // Agrega un evento al botón "Trocar de cor"
+  var colorButton = document.getElementById("colorButton");
+  colorButton.addEventListener("click", toggleColorPicker);
+
+  // Obtén todos los botones de color y agrega un evento a cada uno
+  var colorButtons = document.querySelectorAll(".color-option");
+  colorButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+          var color = button.style.backgroundColor;
+          changeColor(color);
+      });
+  });
+});
+
+function changeColor(color) {
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = color;
   saveBackgroundColor(color);
 }
 
@@ -11,9 +28,15 @@ function saveBackgroundColor(hexadecimalColor) {
 function loadColor() {
   var backgroundColor = localStorage.getItem("background-color");
   if (backgroundColor) {
-    changeColor({ value: backgroundColor }); // Chama changeColor com a cor carregada
+      changeColor(backgroundColor);
   }
 }
 
-// Chama a função loadColor quando a página for carregada para carregar a cor do localStorage
-window.addEventListener("load", loadColor);
+function toggleColorPicker() {
+  var colorSelector = document.getElementById("colors");
+  if (colorSelector.style.display === "none" || colorSelector.style.display === "") {
+      colorSelector.style.display = "block";
+  } else {
+      colorSelector.style.display = "none";
+  }
+}
