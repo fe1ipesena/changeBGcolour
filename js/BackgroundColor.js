8bc06f14509cae0a0cce38268c8f3eeabff895c7
Vitor Eduardo Lopes Francisco <macskids98play@hotmail.com>
2023-09-25 18:46:12 -0300

    pastas

--- js/BackgroundColor.js
+++ js/BackgroundColor.js
@@ -1,11 +1,23 @@
-function toggleColorMenu() {
-  var colorMenu = document.getElementById("colorMenu");
-  colorMenu.style.display =
-    colorMenu.style.display === "none" ? "block" : "none";
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // Llama a la función para cargar el color al cargar la página
+  loadColor();
+
+  // Agrega un evento al botón "Trocar de cor"
+  var colorButton = document.getElementById("colorButton");
+  colorButton.addEventListener("click", toggleColorPicker);
+
+  // Obtén todos los botones de color y agrega un evento a cada uno
+  var colorButtons = document.querySelectorAll(".color-option");
+  colorButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+          var color = button.style.backgroundColor;
+          changeColor(color);
+      });
+  });
+});
 
 function changeColor(color) {
-  document.body.style.backgroundColor = color;
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = color;
   saveBackgroundColor(color);
 }
 
@@ -15,5 +27,16 @@ function saveBackgroundColor(hexadecimalColor) {
 
 function loadColor() {
   var backgroundColor = localStorage.getItem("background-color");
-  changeColor(backgroundColor);
+  if (backgroundColor) {
+      changeColor(backgroundColor);
+  }
+}
+
+function toggleColorPicker() {
+  var colorSelector = document.getElementById("colors");
+  if (colorSelector.style.display === "none" || colorSelector.style.display === "") {
+      colorSelector.style.display = "block";
+  } else {
+      colorSelector.style.display = "none";
+  }
 }
--- vitor/README.md
+++
@@ -1,2 +0,0 @@
-# changeBGcolour
-A html, css and javascript based site that have the functionality to change the background colour.
